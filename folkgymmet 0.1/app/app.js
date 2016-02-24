0d812b10676dda1e63a60a13f3d0931f43888857
Simon Karlsson <simon.karlsson@electrolux.com>
2016-02-24 17:22:33 +0100

    working ui.router in app.js

--- folkgymmet 0.1/app/app.js
+++ folkgymmet 0.1/app/app.js
@@ -1,2 +1,20 @@
-var myApp = angular.module('myApp', [])
-.controller('myController', []);
+var myApp = angular.module('app', ['ui.router','home.module']);
+	myApp.controller('appController', function() {
+		
+		var appCont = this;
+		appCont.shit = "hhh";
+		/* $scope.shit = "hhh";
+		console.log($scope.shit);
+		console.log("app.js");
+		 *///$scope.double = function(value) { return value * 2; };
+	});
+	
+	myApp.config(function($stateProvider, $urlRouterProvider){
+		$urlRouterProvider.otherwise('/');
+		
+		$stateProvider.state('/home', {
+			url: '/home',
+			//controller: 'appController',
+			templateUrl: 'app/components/partials/home/home.html'
+		})
+	});
---
+++ folkgymmet 0.1/app/components/partials/home/home.module.js
@@ -0,0 +1,4 @@
+angular.module('home.module', [])
+	.controller('homeController', function(){
+		
+	});
