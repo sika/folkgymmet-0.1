443a47ee5175e6e05bce665636932bb69cf767d2
Simon Karlsson <simon.karlsson@electrolux.com>
2016-02-29 19:29:26 +0100

    ng-show with login

--- folkgymmet/app/components/directives/header-login/header-login.js
+++ folkgymmet/app/components/directives/header-login/header-login.js
@@ -1,16 +1,9 @@
-﻿var mHeaderLogin = angular.module('headerLogin.module', []);
-mHeaderLogin.directive('dHeaderLogin', function () {
-    return {
-        restrict: 'A',
-        //scope: true,
-        controller: function ($scope) {
-            //console.log("ss");
-            $scope.username = { name: "yeah" };
-        },
-        templateUrl: 'app/components/directives/header-login/header-login.html'
-    }
+﻿$('document').ready(function(){
+	/* $('#btn-login-init').on('click', function(){
+		alert('clicked');
+		
+	}); */
+	$("button").on("click", function(){
+    alert("The paragraph was clicked.");
 });
-//mHeaderMenu.controller('headerMenuController', function ($scope) {
-//    $scope.test = {name: "fff"};
-//    console.log('headerMenuController');
-//});
+});
---
+++ folkgymmet/app/components/directives/header-login/header-login.module.js
@@ -0,0 +1,13 @@
+﻿var mHeaderLogin = angular.module('header-login.module', ['ngAnimate']);
+mHeaderLogin.directive('dHeaderLogin', function () {
+    return {
+        restrict: 'A',
+        //scope: true,
+        controller: function ($scope) {
+			$scope.loginDetailsDisplay = false;
+        },
+        templateUrl: 'app/components/directives/header-login/header-login.html',
+		link: function (scope, element, attrs) {
+		}
+    }
+});
