6dd4bd6f6a5bb0c8890722e70f34cb729b14858d
sika <simon.noworkemail@gmail.com>
2016-03-12 15:00:11 +0100

    Home; added google map; get it to work (not displaying)

--- folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
+++ folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
@@ -1,4 +1,10 @@
 angular.module('eventJoin.module', [])
-	.controller('eventJoinController', function(){
-		
-	});
+	.controller('eventJoinController', function ($scope) {
+	    function initMap() {
+	        $scope.mapDiv = document.getElementById('googlemap');
+	        $scope.map = new google.maps.Map($scope.mapDiv, {
+	            center: { lat: 44.540, lng: -78.546 },
+	            zoom: 8
+	        });
+        }
+    });
