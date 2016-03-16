0fa236073b87e948b872e66281c459df7a56c995
Simon Karlsson <simon.karlsson@electrolux.com>
2016-03-16 16:45:24 +0100

    job; added some code on Gmap; check out link in email

--- folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
+++ folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
@@ -1,12 +1,15 @@
 angular.module('eventJoin.module', [])
+//'uiGmapgoogle-maps','nemLogging'
 	.controller('eventJoinController', function ($scope) {
 	    var map;
 	    function initMap() {
-	        map = new google.maps.Map(document.getElementById('googlemap'), {
+	        $scope.mapProp = {
                 //karta över sverige
 	            center: { lat: 62.5421031, lng: 19.7477994 },
-	            zoom: 5
-	        });
+	            zoom: 5,
+				cords: {lat: 62.5421031, lng: 19.7477994}
+	        };
+			new google.maps.Map(document.getElementById('googlemap'), $scope.mapProp);
 	    }
 	    angular.element(document).ready(function () {
             //run when DOM is ready
