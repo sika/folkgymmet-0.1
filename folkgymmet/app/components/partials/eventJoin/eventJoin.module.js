45422f4d6dc967ca6de0d746de027b6ce54b2a5e
Simon Karlsson <simon.karlsson@electrolux.com>
2016-03-17 17:06:30 +0100

    job; added marker; setup searching marker

--- folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
+++ folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
@@ -1,18 +1,58 @@
 angular.module('eventJoin.module', [])
 //'uiGmapgoogle-maps','nemLogging'
 	.controller('eventJoinController', function ($scope) {
-	    var map;
+		angular.element(document).ready(function () {
+            //run when DOM is ready
+	        initMap();
+			
+			//createMarkers(markersEvent[0]);
+	    });
+			    
 	    function initMap() {
 	        $scope.mapProp = {
                 //karta över sverige
 	            center: { lat: 62.5421031, lng: 19.7477994 },
 	            zoom: 5,
-				cords: {lat: 62.5421031, lng: 19.7477994}
 	        };
-			new google.maps.Map(document.getElementById('googlemap'), $scope.mapProp);
+			$scope.map = new google.maps.Map(document.getElementById('googlemap'), $scope.mapProp);
+			}
+			function createMarkers(info){
+				console.log("createMarkers function");
+				new google.maps.Marker({
+				map: $scope.map,
+				position: new google.maps.LatLng(info.lat, info.lang),
+				title: "test"
+			}
+        );
 	    }
-	    angular.element(document).ready(function () {
-            //run when DOM is ready
-	        initMap();
-	    });
-    });
+
+    })
+	.directive('domMan', function(){
+		return{
+			restrict: 'A',
+			template: 
+			'Name: ',
+			link: function(scope, element, attr){
+				//createMarkers(markersEvent[0]);
+			}
+		}
+	});	
+	//data
+	var markersEvent = [{
+        city : 'Toronto',
+        desc : 'This is the best city in the world!',
+        lat : 62.5421031,
+        lang : 19.7477994
+	}];
+	/*
+	Todo:
+	- search markers
+		- Explicit sök (tryck "sök" eller Enter för verkställ) - detta för att undvika lagg om nya res. ständigt visas
+		- Indicate results: display list of hits in real time in scroll list below search box
+			- Should be clickable
+		- only display markers fitting search (or just one of one in suggestion-list is clicked)
+	- adding markers
+	- removing markers
+	- changing markers
+	
+	*/
