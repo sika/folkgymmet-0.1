0b346b3e0854a62f273404f2205747199976ec49
Simon Karlsson <simon.karlsson@electrolux.com>
2016-03-18 12:41:34 +0100

    job; working scope.markers; fix datalist for search

--- folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
+++ folkgymmet/app/components/partials/eventJoin/eventJoin.module.js
@@ -1,58 +1,94 @@
 angular.module('eventJoin.module', [])
-//'uiGmapgoogle-maps','nemLogging'
-	.controller('eventJoinController', function ($scope) {
-		angular.element(document).ready(function () {
-            //run when DOM is ready
-	        initMap();
-			
-			//createMarkers(markersEvent[0]);
-	    });
-			    
-	    function initMap() {
-	        $scope.mapProp = {
-                //karta över sverige
-	            center: { lat: 62.5421031, lng: 19.7477994 },
-	            zoom: 5,
-	        };
-			$scope.map = new google.maps.Map(document.getElementById('googlemap'), $scope.mapProp);
-			}
-			function createMarkers(info){
-				console.log("createMarkers function");
-				new google.maps.Marker({
-				map: $scope.map,
-				position: new google.maps.LatLng(info.lat, info.lang),
-				title: "test"
-			}
-        );
-	    }
+//controller start
+.controller('eventJoinController', function ($scope) {
+	angular.element(document).ready(function () {
+		//run when DOM is ready
+		initMap();
 
-    })
-	.directive('domMan', function(){
-		return{
-			restrict: 'A',
-			template: 
-			'Name: ',
-			link: function(scope, element, attr){
-				//createMarkers(markersEvent[0]);
-			}
+		for (i = 0; i < $scope.markersEvent.length; i++) {
+			initMarkers($scope.markersEvent[i]); //create initial markers
 		}
-	});	
-	//data
-	var markersEvent = [{
-        city : 'Toronto',
-        desc : 'This is the best city in the world!',
-        lat : 62.5421031,
-        lang : 19.7477994
-	}];
-	/*
-	Todo:
-	- search markers
-		- Explicit sök (tryck "sök" eller Enter för verkställ) - detta för att undvika lagg om nya res. ständigt visas
-		- Indicate results: display list of hits in real time in scroll list below search box
-			- Should be clickable
-		- only display markers fitting search (or just one of one in suggestion-list is clicked)
-	- adding markers
-	- removing markers
-	- changing markers
-	
-	*/
+	});
+	//Pre-load document.ready
+	function initMap() {
+		$scope.mapProp = {
+			//karta över sverige
+			center : {
+				lat : 62.5421031,
+				lng : 19.7477994
+			},
+			zoom : 5,
+		};
+		$scope.map = new google.maps.Map(document.getElementById('googlemap'), $scope.mapProp);
+	}
+	function initMarkers(info) {
+		console.log("initMarkers function");
+		new google.maps.Marker({
+			map : $scope.map,
+			position : new google.maps.LatLng(info.lat, info.lang),
+			title : "test"
+		});
+	}
+	$scope.markersEvent = [{
+		city : 'Göteborg',
+		desc : 'This is the best city in the world!',
+		lat : 57.716610,
+		lang : 11.973904
+	}, {
+		city : 'Stockholm',
+		desc : 'Shit city',
+		lat : 59.336574,
+		lang : 18.067879
+	}, {
+		city : 'Kiruna',
+		desc : 'Shit city',
+		lat : 67.858475,
+		lang : 20.225530
+	}
+];
+})
+//END controller
+//Directive start
+.directive('domMan', function () {
+	return {
+		restrict : 'A',
+		template :
+		'Name: ',
+		link : function (scope, element, attr) {
+			//createMarkers(markersEvent[0]);
+		}
+	}
+});
+//END Directive
+//data
+/*
+var markersEvent = [{
+		city : 'Göteborg',
+		desc : 'This is the best city in the world!',
+		lat : 57.716610,
+		lang : 11.973904
+	}, {
+		city : 'Stockholm',
+		desc : 'Shit city',
+		lat : 59.336574,
+		lang : 18.067879
+	}, {
+		city : 'Kiruna',
+		desc : 'Shit city',
+		lat : 67.858475,
+		lang : 20.225530
+	}
+];
+*/
+/*
+Todo:
+- search markers
+- Explicit sök (tryck "sök" eller Enter för verkställ) - detta för att undvika lagg om nya res. ständigt visas
+- Indicate results: display list of hits in real time in scroll list below search box
+- Should be clickable
+- only display markers fitting search (or just one of one in suggestion-list is clicked)
+- adding markers
+- removing markers
+- changing markers
+
+*/
