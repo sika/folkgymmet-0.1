e48d603dc3d6d1cdafaa422626405809222eeb52
Simon Karlsson <simon.karlsson@electrolux.com>
2016-04-11 16:59:50 +0200

    job; fixed google-service with markers and zoom; app.js make all variables as mapOptions

--- folkgymmet/app/app.js
+++ folkgymmet/app/app.js
@@ -1,135 +1,114 @@
 var myApp = angular.module('app', ['ui.router', 'eventJoin.module', 'eventCreate.module', 'header-menu.module',
-'header-login.module', 'event-type-choose.module', 'sidebar-event-list.module', 'search-bar.module', 'google-map.module']);
+			'header-login.module', 'event-type-choose.module', 'sidebar-event-list.module', 'search-bar.module', 'google-map.module']);
 myApp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+	$urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('eventJoin', {
-        url: '/',
-        controller: 'eventJoinController',
-        templateUrl: 'app/components/partials/eventJoin/eventJoin.html'
-    }).state('eventCreate', {
-        url: '/eventCreate',
-        templateUrl: 'app/components/partials/eventCreate/eventCreate.html'
-    }).state('discussion', {
-        url: '/discussion',
-        templateUrl: 'app/components/partials/discussion/discussion.html'
-    })
+	$stateProvider.state('eventJoin', {
+		url : '/',
+		controller : 'eventJoinController',
+		templateUrl : 'app/components/partials/eventJoin/eventJoin.html'
+	}).state('eventCreate', {
+		url : '/eventCreate',
+		templateUrl : 'app/components/partials/eventCreate/eventCreate.html'
+	}).state('discussion', {
+		url : '/discussion',
+		templateUrl : 'app/components/partials/discussion/discussion.html'
+	})
 });
 myApp.controller('appController', function ($scope) {
-    console.log("appController:");
-    console.log($scope.$id);
-    //$scope.markersEvent = appFactory.getInitMarkers(); //get marker objects from app.js factory
+	console.log("appController:");
+	console.log($scope.$id);
+	//$scope.markersEvent = appFactory.getInitMarkers(); //get marker objects from app.js factory
 })
 myApp.factory('googleFactory', function () {
-    var factoryMap = {};
+	var factoryMap = {};
 
-    //var eventsAll = appFactory.getEventsAll();
-    //factoryMap.get
+	var googleMarkers = [];
+	var hiddenMarkers = [];
 
-    var googleMarkers = [];
-    var hiddenMarkers = [];
-    factoryMap.setGoogleMarker = function (objMarker) {
-        //console.log("setGoogleMarkers");
-        googleMarkers.push(objMarker);
-    }
-    factoryMap.getGoogleMarkers = function () {
-        return googleMarkers;
-    }
-    factoryMap.removeMarkers = function () {
-        googleMarkers = [];
-    }
-    factoryMap.getHiddenMarkers = function () {
-        return hiddenMarkers;
-    }
-    factoryMap.setHiddenMarker = function (arrGoogleMarkers) {
-        /*
-        for (i = 0; i < googleMarkers.length; i++) {
-            if (googleMarkers[i].id == googleMarkerId)
-                googleMarkers[i].setVisible(false);
-        }
-        */
-        hiddenMarkers = arrGoogleMarkers;
-        //objGoogleMarker.setVisible(true);
-    }
-    var mapOptions = {
-        //karta över sverige
-        center: {
-            lat: 62.5421031,
-            lng: 19.7477994
-        },
-        zoom: 5,
-    };
-    factoryMap.getOptions = function () {
-        return mapOptions;
-    }
+	factoryMap.mapOptions = {
+		//karta över sverige
+		center : {
+			lat : 62.5421031,
+			lng : 19.7477994
+		},
+		zoom : 5,
+	};
+	factoryMap.setMapOptions = function(bounds, zoom){ //re-set mapOptions upon searching events (markers)
+		this.mapOptions.center.lat = bounds.getCenter().lat();
+		this.mapOptions.center.lng = bounds.getCenter().lng();
+		this.mapOptions.zoom = zoom;		
+	}
+	factoryMap.getGoogleMarkers = function () {
+		return googleMarkers;
+	}
+	factoryMap.removeMarkers = function () {
+		googleMarkers = [];
+	}
+	factoryMap.getHiddenMarkers = function () {
+		return hiddenMarkers;
+	}
+	factoryMap.setHiddenMarker = function (arrGoogleMarkers) {
+		hiddenMarkers = arrGoogleMarkers;
+	}
 
-    return factoryMap;
-    /*
-    factoryMap.setMarker = function (objCurrentMarkers) {
-        googleMarkers = objCurrentMarkers;
-    }
+	/*
+	factoryMap.setZoom = function(objBounds){
+		googleBounds = objBounds;
+	}
+	*/
+	return factoryMap;
 
-    factoryMap.getMarkers = function () {
-        return googleMarkers;
-    }
-
-    
-    factoryMap.resetMarkers = function (){
-        var resetMarkers = appFactory.getEventsAll(); //put in all events as markers
-        return resetMarkers;
-    }
-
-     
-    factoryMap.setOptions = function (objMapOptions) {
-        //return mapOptions;
-    }
-    
-    */
-    
+	/*
+	factoryMap.setGoogleMarker = function (objGoogleMarker) {
+	googleMarkers.push(objGoogleMarker);
+	}
+	 */
 
 })
 /*factory declaration*/
 myApp.factory('appFactory', function () {
-    /*empty object declaration*/
-    var factory = {};
-    var eventsAll = [{
-        id: 1,
-        label: "göteborg yeah",
-        city: 'Göteborg',
-        desc: 'This is the best city in the world!',
-        lat: 57.716610,
-        lang: 11.973904
-    }, {
-        id: 2,
-        label: "sthlm",
-        city: 'Stockholm',
-        desc: 'This is Stockholm',
-        lat: 59.336574,
-        lang: 18.067879
-    }, {
-        id: 3,
-        label: "kirri yeah",
-        city: 'Kiruna',
-        desc: 'This is Kiruna',
-        lat: 67.858475,
-        lang: 20.225530
-    }];
+	/*empty object declaration*/
+	var factory = {};
+	var eventsAll = [{
+			id : 1,
+			label : "göteborg yeah",
+			city : 'Göteborg',
+			desc : 'This is the best city in the world!',
+			lat : 57.716610,
+			lang : 11.973904
+		}, {
+			id : 2,
+			label : "sthlm",
+			city : 'Stockholm',
+			desc : 'This is Stockholm',
+			lat : 59.336574,
+			lang : 18.067879
+		}, {
+			id : 3,
+			label : "kirri yeah",
+			city : 'Kiruna',
+			desc : 'This is Kiruna',
+			lat : 67.858475,
+			lang : 20.225530
+		}
+	];
 
-    var visibleMarkers = eventsAll; //initate visible markers will all markers/events
+	var visibleMarkers = eventsAll; //initate visible markers will all markers/events
 
-    
-    factory.getEventsAll = function () {
-        //console.log(markersEvent);
-        return eventsAll;
-    };
-       
-    factory.getVisibleMarkers = function () {
-        return visibleMarkers;
-    }
-    factory.setVisibleMarkers = function (arrMarker) {
-        visibleMarkers = arrMarker;
-    }
-    
-    return factory;
-    
+
+	factory.getEventsAll = function () {
+		//console.log(markersEvent);
+		return eventsAll;
+	};
+
+	factory.getVisibleMarkers = function () {
+		return visibleMarkers;
+	}
+	factory.setVisibleMarkers = function (arrMarker) {
+		visibleMarkers = arrMarker;
+	}
+
+	return factory;
+
 }); //appFactory END
--- folkgymmet/app/components/directives/google-map/google-map.module.js
+++ folkgymmet/app/components/directives/google-map/google-map.module.js
@@ -1,17 +1,13 @@
 ﻿angular.module('google-map.module', ['search-bar.module'])
 .controller('google-map.controller', function ($scope, appFactory, searchFactory, googleFactory) {
-    $scope.apiMap = googleFactory;
-    $scope.apiApp = appFactory;
-    $scope.mapOptions = $scope.apiMap.getOptions();
-    $scope.events = $scope.apiApp.getEventsAll();    
-    $scope.apiSearch = searchFactory;
-    /*
-    $scope.appFactory = appFactory;
-	$scope.eventsAll = appFactory.getEventsAll(); //get marker objects from app.js factory using parent scope
-	$scope.visibleMarkers = appFactory.getVisibleMarkers(); //get visible markers
-	$scope.gMarkers = appFactory.getGoogleMarkers();
-	console.log($scope.gMarkers);
-    */
+	$scope.apiMap = googleFactory;
+	$scope.apiApp = appFactory;
+	$scope.apiSearch = searchFactory;
+	$scope.mapOptions = $scope.apiMap.mapOptions;
+	console.log($scope.mapOptions);
+	$scope.events = $scope.apiApp.getEventsAll(); //events to base new markers on (long/lat)
+	$scope.gMarkers = []; //google markers array
+	$scope.bounds = $scope.apiMap.getBounds();
 })
 .directive('dGoogleMap', function () {
 	return {
@@ -23,86 +19,83 @@
 			//var gMarkers = [];
 			angular.element(document).ready(function () {
 				//run when DOM is ready
-			    loadMap();
-			    scope.apiMap.removeMarkers(); //remove all markers; otherwise there will be duplicates (re-adding already added in setMarkers() )
+				createMap();
 				for (i = 0; i < scope.events.length; i++) {
-				    setMarkers(scope.events[i]); //set markers
+					createMarkers(scope.events[i]); //(re-)create new markers
 				}
-				scope.markers = scope.apiMap.getGoogleMarkers();
 				scope.hiddenMarkers = scope.apiMap.getHiddenMarkers();
-				console.log(scope.hiddenMarkers);
-			    hideMarkers(scope.markers, scope.hiddenMarkers); //hide markers if earlier set to hidden
+				hideMarkers(scope.gMarkers, scope.hiddenMarkers); //hide gMarkers if earlier set to hidden
 			});
-			function loadMap() {
-			    scope.map = new google.maps.Map(document.getElementById(attr.id), scope.mapOptions);
+			function createMap() {
+				scope.map = new google.maps.Map(document.getElementById(attr.id), scope.mapOptions);
+				//scope.map.fitBounds(scope.bounds);
 			}
-			function setMarkers(event) {
+			function createMarkers(event) {
 				marker = new google.maps.Marker({
 						map : scope.map,
-						position: new google.maps.LatLng(event.lat, event.lang),
-						id: event.id
+						position : new google.maps.LatLng(event.lat, event.lang),
+						id : event.id
 						//title: "test"
 					});
-			    //gMarkers.push(marker);
-				scope.apiMap.setGoogleMarker(marker);
+				scope.gMarkers.push(marker);
 			}
 			function hideMarkers(markers, hiddenMarkers) { //hide markers
-			    for (i = 0; i < markers.length; i++) {
-			        for (j = 0; j < hiddenMarkers.length; j++)
-			            if (markers[i].id == hiddenMarkers[j].id) {
-			                markers[i].setVisible(false);
-			            }
-			    }
-			    scope.apiMap.setHiddenMarker(markers); //set hidderMarkers to new list
+			//console.log(hiddenMarkers)			
+			var visible;
+				for (i = 0; i < markers.length; i++) {
+					for (j = 0; j < hiddenMarkers.length; j++)
+						if (markers[i].id == hiddenMarkers[j].id) {
+							visible = hiddenMarkers[j].getVisible();
+							markers[i].setVisible(visible);
+						}
+				}
+				scope.apiMap.setHiddenMarker(markers); //set hidderMarkers to new list
 			}
 			scope.$watch('apiSearch.filteredMarkers', function (newValue, oldValue) {
-			    console.log("search watch");
-			    if (newValue !== oldValue) {
-			        console.log("search watch changed");
-				    markersSearchResult(scope.apiSearch.filteredMarkers);
+				if (newValue !== oldValue) {
+					markersSearchResult(scope.apiSearch.filteredMarkers);
 				}
 			});
-			function markersSearchResult(arrMarkers) {
-				var arrMarkersTmp = [];
-				if (arrMarkers.length == 0) { //array is empty (search empty)
-				    for (i = 0; i < scope.markers.length; i++) {
-				        scope.markers[i].setVisible(true); //set all markers to visible
-				    }
-				    scope.apiMap.setHiddenMarker(markers); //hidden markers will all be visible
-				    //zoomToMarkers(markers);
-				} else {
-				    for (i = 0; i < scope.markers.length; i++) {
-						scope.markers[i].setVisible(false); //hide all markers before
+			function markersSearchResult(arrSearchMarkers) {
+				var arrSearchMarkersTmp = [];
+				if (arrSearchMarkers.length == 0) { //array is empty (search empty)
+					for (i = 0; i < scope.gMarkers.length; i++) {
+						scope.gMarkers[i].setVisible(true); //set all markers to visible
 					}
-					for (i = 0; i < scope.markers.length; i++) {
-						for (j = 0; j < arrMarkers.length; j++) {
-							if (arrMarkers[j].id == scope.markers[i].id) {
-							    scope.markers[i].setVisible(true);
-							    arrMarkersTmp.push(scope.markers[i]);
+					scope.apiMap.setHiddenMarker(scope.gMarkers); //hidden markers will all be visible
+					//zoomToMarkers(markers);
+				} else {
+					for (i = 0; i < scope.gMarkers.length; i++) {
+						scope.gMarkers[i].setVisible(false); //hide all markers before
+					}
+					for (i = 0; i < scope.gMarkers.length; i++) {
+						for (j = 0; j < arrSearchMarkers.length; j++) {
+							if (arrSearchMarkers[j].id == scope.gMarkers[i].id) {
+								scope.gMarkers[i].setVisible(true);
+								arrSearchMarkersTmp.push(scope.gMarkers[i]); //gMarkers where visible is true
 								break;
 							}
-						}
-					}
-					scope.apiMap.setHiddenMarker(scope.markers);
-					zoomToMarkers(arrMarkersTmp);
+						}						
+					}					
+					scope.apiMap.setHiddenMarker(scope.gMarkers); //pass gMarkers to set which markers are hidden
+					zoomToMarkers(arrSearchMarkersTmp);
 				}
 			} //markersSearchResult END
-			function zoomToMarkers(arrMarkersTmp) {
-                /*
-				var markers = arrMarkersTmp;
+			function zoomToMarkers(arrSearchMarkersTmp) {
+				var markersVisible = arrSearchMarkersTmp;
 				var bounds = new google.maps.LatLngBounds();
-				for (var i = 0; i < markers.length; i++) {
-					bounds.extend(markers[i].getPosition());
+				for (var i = 0; i < markersVisible.length; i++) {
+					bounds.extend(markersVisible[i].getPosition());
 				}
 				scope.map.fitBounds(bounds);
-				//remove one zoom level to ensure no marker is on the edge.
-				//scope.map.setZoom(scope.map.getZoom() - 1);
 				// set a minimum zoom
 				// if you got only 1 marker or all markers are on the same address map will be zoomed too much.
+				
 				if (scope.map.getZoom() > 15) {
 					scope.map.setZoom(15);
 				}
-                */
+				var zoom = scope.map.getZoom();
+				scope.apiMap.setMapOptions(bounds, zoom); //save new mapOptions in factory
 			} //zoomToMarkers END
 		} //Link END
 	} //return END
